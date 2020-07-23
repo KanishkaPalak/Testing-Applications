7c960258a00b495c26e3764b275f4736261f7aa1
Kanishka Rao <kanishka.palak@gmail.com>
2020-07-23 21:53:48 +0530

    Update ProfileController.java

--- ehealthcareproject/src/main/java/com/paytm/learnwebapp/controller/ProfileController.java
+++ ehealthcareproject/src/main/java/com/paytm/learnwebapp/controller/ProfileController.java
@@ -5,8 +5,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-public class ProfileController {
+@RestController
+public class UserController {
 
 	@RequestMapping(value = "/home", method = RequestMethod.GET)
 	public String homepage()
