56ce10ad4ff64c00cb2baebf455e2ef982368c7d
Kanishka Rao <kanishka.palak@gmail.com>
2020-07-23 20:20:39 +0530

    Add files via upload

---
+++ ehealthcareproject/src/main/java/com/paytm/learnwebapp/ehealthcareproject/EHealthCareProjectApplication.java
@@ -0,0 +1,15 @@
+package com.paytm.learnwebapp.ehealthcareproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+public class EHealthCareProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EHealthCareProjectApplication.class, args);
+	}
+
+}
