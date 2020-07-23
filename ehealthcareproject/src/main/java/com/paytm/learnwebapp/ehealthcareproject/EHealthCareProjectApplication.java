aea4be55a37125d3bc115dda6e077f0284243894
Kanishka Rao <kanishka.palak@gmail.com>
2020-07-23 21:58:18 +0530

    Update EHealthCareProjectApplication.java

--- ehealthcareproject/src/main/java/com/paytm/learnwebapp/ehealthcareproject/EHealthCareProjectApplication.java
+++ ehealthcareproject/src/main/java/com/paytm/learnwebapp/ehealthcareproject/EHealthCareProjectApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+@ComponentScan(basePackages= {"com.paytm.learnwebapp"})
 public class EHealthCareProjectApplication {
 
 	public static void main(String[] args) {
